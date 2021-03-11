da52bbcdc8ae15f06952d2b6dcec0b7c43377287
IrmaIvankovic <ivankovicirma@gmail.com>
2021-03-11 21:46:21 +0100

    Decimal Numbers

---
+++ ex04/src/index.js
@@ -0,0 +1,17 @@
+function main(){
+    var myDecimal;
+    var myDecimalE;
+
+    myDecimal = 8.2;
+    myDecimalE = 254e-5;
+
+    return {
+    myDecimal,
+    myDecimalE
+
+    };
+
+}
+
+console.log(main());
+module.exports = main;
