5d48410caa3285cb528b3fc5c9479b2ed5d39c07
IrmaIvankovic <ivankovicirma@gmail.com>
2021-03-11 23:04:50 +0100

    Making a remainder!

---
+++ ex07/src/index.js
@@ -0,0 +1,17 @@
+function main (numOne, numTwo) {
+    var numOne; 
+    var numTwo; 
+    var remainder = numOne % numTwo;
+
+    parseInt (numOne);
+    parseInt (numTwo);
+    
+      return remainder;  
+
+    }
+       
+   
+    console.log(main(5, 2));
+    console.log(main(15, 4));
+    console.log(main(102, 25));
+    module.exports = main
