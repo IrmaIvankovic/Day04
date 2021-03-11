c41d1b0c356c03422a866a2918d6adf10aa46381
IrmaIvankovic <ivankovicirma@gmail.com>
2021-03-11 23:47:28 +0100

    Strings

---
+++ ex10/src/index.js
@@ -0,0 +1,17 @@
+function main () {
+    var myFirstName = "Irma";
+    var myLastName = "Ivanković";
+    var myHomeTown = "Sarajevo";
+
+
+    return {
+    myFirstName,
+    myLastName,
+    myHomeTown
+
+    }
+
+    }
+       
+    console.log(main());
+    module.exports = main
